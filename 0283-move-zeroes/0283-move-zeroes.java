a7c088eeede382c82565bb55dfc563c5355c1514
OGRE1000 <142719539+OGRE1000@users.noreply.github.com>
2025-12-24 21:09:47 +0530

    Time: 2 ms (88.07%), Space: 47.9 MB (12.59%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int k = 0; // position for next non-zero element
+
+        // Move all non-zero elements to the front
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[k++] = nums[i];
+            }
+        }
+
+        // Fill remaining positions with zero
+        while (k < nums.length) {
+            nums[k++] = 0;
+        }
+    }
+}
