05699ab94491ddb83964dcfddfff980288be017f
OGRE1000 <142719539+OGRE1000@users.noreply.github.com>
2025-12-24 23:45:19 +0530

    Time: 23 ms (77.06%), Space: 76.4 MB (56.24%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            // If duplicate found inside window
+            if (set.contains(nums[right])) {
+                return true;
+            }
+
+            set.add(nums[right]);
+
+            // Shrink window if size exceeds k
+            if (right - left >= k) {
+                set.remove(nums[left]);
+                left++;
+            }
+        }
+        return false;
+    }
+}
